a4d7e9ed71f7325d9247a3eb00865a2704fb8101
Rich Ives <rjsives89@gmail.com>
2022-09-22 22:29:36 -0500

    Added descriptive comments to main - New branch for controller exercise

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -9,8 +9,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("hello")
 
 public class HelloController {
-// testing
+    // Handle request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
 
+    // lives at /hello/goodbye
         @GetMapping("goodbye")
         public String goodbye(){
 
@@ -43,11 +49,12 @@ public class HelloController {
 //                    "<html>";
 //    }
 
+    // /hello/form
         @GetMapping("form")
     public String helloForm(){
             return "<html>" +
                     "<body>" +
-                    "<form action = 'hello' method= 'post'>" +
+                    "<form action = 'hello' method= 'post'>" + // submit a request to /hello
                     "<input type='text' name='name'>" +
                     "<input type= 'submit' value= 'Greet me!'>" +
                     "</form>" +
